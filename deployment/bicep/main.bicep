1d62c5437cae7dacb293d7281476fd6a1512e817
ahmeds <ahmeds@ms.com>
2021-08-18 10:30:25 +0200

    fixed naming of password and username

--- deployment/bicep/main.bicep
+++ deployment/bicep/main.bicep
@@ -94,8 +94,8 @@ module shared './shared/shared.bicep' = {  dependsOn: [
     sharedResourceGroupResources : sharedResourceGroupResources
     jumpboxSubnetId: jumpboxSubnetId
     agentSubnetId: agentSubnetId
-    vmazdevopsPassword: vmPassword
-    vmazdevopsUsername: vmUsername
+    vmdevopsPassword: vmPassword
+    vmdevopsUsername: vmUsername
     personalAccessToken: personalAccessToken
     accountname: accountName
     orgtype: orgtype
--- deployment/bicep/testpscript.ps1
+++ deployment/bicep/testpscript.ps1
@@ -1,15 +1,14 @@
-$RESOURCE_GROUP = "tesatase2"
 $LOCATION = "westeurope"
 $BICEP_FILE="main.bicep"
 
-# delete a deployment
-az deployment sub  delete  --name testasedeployment
+# set this to file with parameter. localparam*.json is not committed to git
+$PARAM_FILE="localparamgh.json"
 
-# deploy the bicep file directly
+
 
 # make a copy of parameters json file. localparam.json is not committed to repo
 # put the variables you need in localparam.json
 copy parameters.json localparam.json
 
 
-az deployment sub  create --name testasedeployment2   --template-file $BICEP_FILE   --parameters localparam.json --location $LOCATION -o json
+az deployment sub  create --name testasedeployment7   --template-file $BICEP_FILE   --parameters $PARAM_FILE --location $LOCATION -o json
