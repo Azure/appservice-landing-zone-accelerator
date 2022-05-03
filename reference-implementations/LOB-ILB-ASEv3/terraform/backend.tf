e5a419af11fa791b704731cb4c735542d9c2bf27
Jin Lee <jinle@microsoft.com>
2022-05-02 21:57:47 -0500

    terraform backend configuration

--- reference-implementations/LOB-ILB-ASEv3/terraform/backend.tf
+++ reference-implementations/LOB-ILB-ASEv3/terraform/backend.tf
@@ -6,8 +6,8 @@ terraform {
     }
   }
   backend "azurerm" {
-      resource_group_name  = "tfstate"
-      storage_account_name = "<storage_account_name>"
+      resource_group_name  = "backend-appsrvc-dev-westus2-001"
+      storage_account_name = "stbackendappsrwestus2001"
       container_name       = "tfstate"
       key                  = "terraform.tfstate"
   }
