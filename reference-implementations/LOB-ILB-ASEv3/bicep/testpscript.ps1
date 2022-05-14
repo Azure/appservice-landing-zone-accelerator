b73b80e396935010b9d274506a9df041847ec60f
Nikolaos Antoniou <nianton@gmail.com>
2022-05-14 18:42:18 +0300

    Bicep: parameterized numeric suffix

--- reference-implementations/LOB-ILB-ASEv3/bicep/testpscript.ps1
+++ reference-implementations/LOB-ILB-ASEv3/bicep/testpscript.ps1
@@ -1,5 +1,6 @@
 $LOCATION = "westeurope"
 $BICEP_FILE="main.bicep"
+$DEPLOYMENT_NAME=('ase-' + (Get-Date -Format "yyyyMMdd-HHmmss"))
 
 # make a copy of parameters json file. localparam.json is not committed to repo
 # put the variables you need in localparam.json and then reference it in the $PARAM_FILE
@@ -8,4 +9,4 @@ $BICEP_FILE="main.bicep"
 # set this to file with parameter. localparam*.json is not committed to git
 $PARAM_FILE="localparam.json"
 
-az deployment sub  create --name ('ase-' + (Get-Date -Format "yyyyMMdd-HHmmss"))   --template-file $BICEP_FILE   --parameters $PARAM_FILE --location $LOCATION -o json
+az deployment sub create --name $DEPLOYMENT_NAME --template-file $BICEP_FILE --parameters $PARAM_FILE --location $LOCATION -o json
