f77719b1a8640c1ddbe7061db74a79c6e6459d2a
roberto.perez <robertopc@gmail.com>
2022-07-29 13:03:03 -0400

    Adding extension classes for Registering Services and Registering Middlewares

---
+++ sampleapp/SampleAppService/RegisterServices.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Options;
+using StackExchange.Redis;
+
+namespace SampleAppService;
+
+public static class RegisterStartupServices
+{
+
+    public static WebApplicationBuilder RegisterServices(this WebApplicationBuilder builder)
+    {
+        //Add Options Pattern
+        builder.Services.AddOptions();
+        builder.Services.AddOptions<SampleAppServiceOptions>().Bind(builder.Configuration.GetSection(SampleAppServiceOptions.Section));
+
+        var sp = builder.Services.BuildServiceProvider();
+        var options = sp.GetService<IOptions<SampleAppServiceOptions>>();
+
+        if (options != null && options.Value.AddRedis)
+        {
+            //Configure Redis
+            var endpoint = options.Value.GetRedisEnpoint();
+            var redisConnection = ConnectionMultiplexer.Connect(endpoint);
+            builder.Services.AddSingleton<IConnectionMultiplexer>(redisConnection);
+        }
+
+        // Add services to the container.
+        builder.Services.AddRazorPages();
+        builder.Services.AddHealthChecks().AddCheck("default", () => {
+            // TODO: need "real world" validation for dependencies
+            // see https://docs.microsoft.com/en-us/azure/architecture/patterns/health-endpoint-monitoring
+            return HealthCheckResult.Healthy();
+        });
+
+        return builder;
+    }
+}
+
