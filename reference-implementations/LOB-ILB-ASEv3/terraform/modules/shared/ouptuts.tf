a20e8857fdd22b862fed40d57dda4ae535762485
Jin Lee <jinle@microsoft.com>
2022-08-08 09:45:09 -0500

    debugging shared-vm modules

--- reference-implementations/LOB-ILB-ASEv3/terraform/modules/shared/ouptuts.tf
+++ reference-implementations/LOB-ILB-ASEv3/terraform/modules/shared/ouptuts.tf
@@ -1,7 +1,21 @@
 output "instrumentation_key" {
   value = azurerm_application_insights.appinsights.instrumentation_key
+  sensitive = true
 }
 
 output "app_id" {
   value = azurerm_application_insights.appinsights.app_id
+}
+
+output "jumpbox_vm_id" {
+  value = module.jumpboxvm.id
+}
+output "devops_vm_id" {
+  value = module.devopsvm.id
+}
+output "jumpbox_vm_private_ip" {
+  value = module.jumpboxvm.private_ip_address
+}
+output "devops_vm_private_ip" {
+  value = module.devopsvm.private_ip_address
 }
