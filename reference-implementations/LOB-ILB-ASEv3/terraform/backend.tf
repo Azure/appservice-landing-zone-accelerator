f11e5d3114fe60a7e740a495bee5d1aeee3e0b0f
Jin Lee <jinle@microsoft.com>
2022-08-08 09:23:15 -0500

    adding shared vm module

--- reference-implementations/LOB-ILB-ASEv3/terraform/backend.tf
+++ reference-implementations/LOB-ILB-ASEv3/terraform/backend.tf
@@ -5,10 +5,10 @@ terraform {
       version = "~> 3.1"
     }
   }
-  backend "azurerm" {
-    resource_group_name  = "backend-appsrvc-dev-westus2-001"
-    storage_account_name = "stbackendappsrwestus2001"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-  }
+  # backend "azurerm" {
+  #   resource_group_name  = "backend-appsrvc-dev-westus2-001"
+  #   storage_account_name = "stbackendappsrwestus2001"
+  #   container_name       = "tfstate"
+  #   key                  = "terraform.tfstate"
+  # }
 }
