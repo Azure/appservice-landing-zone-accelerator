c33e0860c273002e7014c1c66cc724ea7802cfbb
Jin Lee <jinle@microsoft.com>
2022-11-02 15:28:01 -0500

    debugging terraform action yml

--- reference-implementations/LOB-ILB-ASEv3/terraform/backend.tf
+++ reference-implementations/LOB-ILB-ASEv3/terraform/backend.tf
@@ -1,8 +1,10 @@
 terraform {
+  required_version = ">=1.2"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.1"
+      version = "~>3.7.0"
     }
   }
   backend "azurerm" {
