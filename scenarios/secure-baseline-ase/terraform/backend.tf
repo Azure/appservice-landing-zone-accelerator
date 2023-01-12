bfeaaf471187e93925d40ef59bf55396823679fc
Jin Lee <jinle@microsoft.com>
2023-01-12 10:37:19 -0600

    updating cicd pipelines

--- scenarios/secure-baseline-ase/terraform/backend.tf
+++ scenarios/secure-baseline-ase/terraform/backend.tf
@@ -8,11 +8,11 @@ terraform {
     }
   }
   backend "azurerm" {
-    resource_group_name  = "backend-appsrvc-dev-westus2-001"
-    storage_account_name = "stbackendappsrwestus2001"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
   }
+  # resource_group_name  = "backend-appsrvc-dev-westus2-001"
+  # storage_account_name = "stbackendappsrwestus2001"
+  # container_name       = "tfstate"
+  # key                  = "terraform.tfstate"
 }
 
 provider "azurerm" {
