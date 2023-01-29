88f9550c0b4ccf0003c1fe95911958d717847538
Daniel Mossberg <5992827+dmossberg@users.noreply.github.com>
2023-01-29 11:31:42 +0100

    Merge from main

--- scenarios/secure-baseline-ase/terraform/backend.tf
+++ scenarios/secure-baseline-ase/terraform/backend.tf
@@ -1,14 +1,19 @@
 terraform {
+  required_version = ">=1.2" # must be greater than or equal to 1.2 for OIDC
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.1"
+      version = "~>3.7.0"
     }
   }
-  backend "azurerm" {
-    resource_group_name  = "backend-appsrvc-dev-westus2-001"
-    storage_account_name = "stbackendappsrwestus2001"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-  }
+  backend "azurerm" {}
+}
+
+provider "azurerm" {
+  features {}
+
+  # DO NOT CHANGE THE BELOW VALUES
+  disable_terraform_partner_id = false
+  partner_id                   = "cf7e9f0a-f872-49db-b72f-f2e318189a6d"
 }
