3929c15edd854ef9553c7eb4d75d76a05578895d
Jin Lee <jinle@microsoft.com>
2023-02-19 17:38:43 -0600

    debugging tf plan build failure

--- scenarios/secure-baseline-ase/terraform/backend.tf
+++ scenarios/secure-baseline-ase/terraform/backend.tf
@@ -1,19 +1,21 @@
 terraform {
-  required_version = ">=1.3.7" # must be greater than or equal to 1.2 for OIDC
+  required_version = ">=1.2" # must be greater than or equal to 1.2 for OIDC
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>3.44.0"
+      version = "~>3.7.0"
     }
   }
-  backend "azurerm" {}
+  # backend "azurerm" {}
 }
 
 provider "azurerm" {
   features {}
 
   # DO NOT CHANGE THE BELOW VALUES
+  # ==============================
+  # partner_id: GUID/UUID registered with Microsoft to facilitate partner resource usage attribution
   disable_terraform_partner_id = false
   partner_id                   = "cf7e9f0a-f872-49db-b72f-f2e318189a6d"
 }
