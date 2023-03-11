d8aa2faf550890a0fd9d751ec9216684dfb87ff1
Daniel Mossberg <daniel.mossberg@microsoft.com>
2023-03-11 11:41:05 +0100

    Hopefully working version

--- scenarios/secure-baseline-multitenant/terraform/modules/shared/windows-vm-ext/az-cli-runner.ps1
+++ scenarios/secure-baseline-multitenant/terraform/modules/shared/windows-vm-ext/az-cli-runner.ps1
@@ -3,26 +3,19 @@ param (
     [string]$command
 )
 
-Start-Transcript (".\InstallPSScript.log")
-Write-Host "Received command: $command"
+$Downloaddir = "D:\ac-cli-runner"
+if ((Test-Path -Path $Downloaddir) -ne $true) {
+    mkdir $Downloaddir
+}
 
+$date = Get-Date -Format "yyyyMMdd-HHmmss"
+Start-Transcript ("D:\ac-cli-runner\InstallPSScript-" + $date + ".log")
 
 Write-Host "Installing Azure CLI..."
-# Install the Azure CLI
-Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; 
-Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet';
-Remove-Item .\AzureCLI.msi
+Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile D:\ac-cli-runner\AzureCLI.msi; 
+Start-Process msiexec.exe -Wait -ArgumentList '/I D:\ac-cli-runner\AzureCLI.msi /quiet';
 $env:Path += ";C:\Program Files (x86)\Microsoft SDKs\Azure\CLI2\wbin\"
 
-# Define the list of Azure CLI commands
-# $commands = @(
-#     "az login",
-#     "az keyvault secret set --vault-name kv-appsvc-staging-337 --name mysecret --value mysecretvalue",
-#     "az appconfig kv set --auth-mode login --endpoint https://appcg-sec-baseline-staging-337.azconfig.io --key color --value red --label mylabel -y"
-# )
 
-# Execute each command in the list
-# foreach ($command in $commands) {
-    Write-Host "Executing command: $command"
-    Invoke-Expression $command
-# }
+Write-Host "Executing command: $command"
+Invoke-Expression $command
