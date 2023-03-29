838c03d87535986e7e71cfb0aece846f09fb5f4b
Jin Lee <94473824+JinLee794@users.noreply.github.com>
2023-03-29 09:15:15 -0500

    Feature/107/pipelines refactor (#115)
    Adding conditional logic on the Terraform secure baseline ase deployment pipeline to:
    
    Only execute deployment (tf apply) on push to main events (once PR is merged)
    Added Production environment for Azure OIDC to control approval process for deployments
    Only executes the plan steps on PRs to allow for external contributors to introduce changes without deployment into our environment
    Added a pull request template for introducing changes going forward
    Added conditional logic to the ASE resources on the TF deployment to allow for optional creation of ASE (specify ase name/rg name in the input parameters as shown on Parameters/uat.tfvars)

--- scenarios/secure-baseline-ase/terraform/Parameters/uat.tfvars
+++ scenarios/secure-baseline-ase/terraform/Parameters/uat.tfvars
@@ -1,1 +1,3 @@
 location = "westus2"
+# app_service_environment_name                = "ase-sec-baseline-sgl-dev-westus2-001"
+# app_service_environment_resource_group_name = "rg-asev3-shared"
--- scenarios/secure-baseline-ase/terraform/backend.tf
+++ scenarios/secure-baseline-ase/terraform/backend.tf
@@ -14,6 +14,8 @@ provider "azurerm" {
   features {}
 
   # DO NOT CHANGE THE BELOW VALUES
+  # ==============================
+  # partner_id: GUID/UUID registered with Microsoft to facilitate partner resource usage attribution
   disable_terraform_partner_id = false
   partner_id                   = "cf7e9f0a-f872-49db-b72f-f2e318189a6d"
 }
