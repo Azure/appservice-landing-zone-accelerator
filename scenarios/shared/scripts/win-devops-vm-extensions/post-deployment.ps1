0514b719562c3e7a732d804deb291e2a4b9a2831
Thodoris Theodorou <thotheod@microsoft.com>
2023-05-24 13:13:14 +0300

    fix: add winget installation in script

--- scenarios/shared/scripts/win-devops-vm-extensions/post-deployment.ps1
+++ scenarios/shared/scripts/win-devops-vm-extensions/post-deployment.ps1
@@ -204,6 +204,22 @@ foreach ($download in $downloads) {
     }
 }
 
+# get latest download url for winget-cli
+$URL = "https://api.github.com/repos/microsoft/winget-cli/releases/latest"
+$URL = (Invoke-WebRequest -Uri $URL).Content | ConvertFrom-Json |
+Select-Object -ExpandProperty "assets" |
+Where-Object "browser_download_url" -Match '.msixbundle' |
+Select-Object -ExpandProperty "browser_download_url"
+
+# download
+Invoke-WebRequest -Uri $URL -OutFile "Setup.msix" -UseBasicParsing
+
+# install
+Add-AppxPackage -Path "Setup.msix"
+
+# delete file
+Remove-Item "Setup.msix"
+
 # # Run Azure CLI commands
 
 # if (-not [string]::IsNullOrEmpty($az_cli_commands)) {
