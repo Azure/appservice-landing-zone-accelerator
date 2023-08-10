7ececefe43b422af40d5ef6820c467c0d2b8dea8
Jin Lee <jinle@microsoft.com>
2023-08-10 14:55:52 -0500

    minor config fix for scenario 2

--- scenarios/secure-baseline-ase/terraform/shared.tf
+++ scenarios/secure-baseline-ase/terraform/shared.tf
@@ -3,14 +3,15 @@ module "devops_vm" {
 
   source = "../../shared/terraform-modules/windows-vm"
 
-  resource_group     = azurerm_resource_group.shared.name
-  vm_name            = "devops"
-  location           = var.location
-  vm_subnet_id       = module.vnetHub.subnets["CICDAgentSubnet"].id
-  admin_username     = var.vmAdminUsername
-  admin_password     = var.vmAdminPassword
-  aad_admin_username = var.vm_aad_admin_username
-  global_settings    = local.global_settings
+  resource_group      = azurerm_resource_group.shared.name
+  vm_name             = "devops"
+  location            = var.location
+  vm_subnet_id        = module.vnetHub.subnets["CICDAgentSubnet"].id
+  admin_username      = var.vmAdminUsername
+  admin_password      = var.vmAdminPassword
+  aad_admin_username  = var.vm_aad_admin_username
+  aad_admin_object_id = var.vm_aad_admin_object_id
+  global_settings     = local.global_settings
 
   tags = local.base_tags
 
@@ -25,13 +26,13 @@ module "jumpbox_vm" {
 
   source = "../../shared/terraform-modules/windows-vm"
 
-  resource_group      = azurerm_resource_group.shared.name
-  vm_name             = "jumpbox"
-  location            = var.location
-  vm_subnet_id        = module.vnetHub.subnets["JumpBoxSubnet"].id
-  admin_username      = var.vmAdminUsername
-  admin_password      = var.vmAdminPassword
-  aad_admin_username  = var.vm_aad_admin_username
+  resource_group = azurerm_resource_group.shared.name
+  vm_name        = "jumpbox"
+  location       = var.location
+  vm_subnet_id   = module.vnetHub.subnets["JumpBoxSubnet"].id
+  admin_username = var.vmAdminUsername
+  admin_password = var.vmAdminPassword
+  # aad_admin_username  = var.vm_aad_admin_username
   aad_admin_object_id = var.vm_aad_admin_object_id
   global_settings     = local.global_settings
 
