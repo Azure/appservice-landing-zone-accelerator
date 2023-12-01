c0b89ecb1d688d4919bf9e0c070ede84a7936656
dantelmomsft <dantelmo@microsoft.com>
2023-12-01 12:42:02 +0100

    fix vm extension script for azd install

--- scenarios/shared/scripts/win-devops-vm-extensions/test-vm-extension.sh
+++ scenarios/shared/scripts/win-devops-vm-extensions/test-vm-extension.sh
@@ -1,18 +1,9 @@
 # /bin/bash
 
-commandToExecute='powershell.exe -ExecutionPolicy Unrestricted -File post-deployment.ps1 -az_cli_commands "az version" -install_ssms'
-fileUris="'https://raw.githubusercontent.com/Azure/appservice-landing-zone-accelerator/main/scenarios/shared/scripts/win-devops-vm-extensions/post-deployment.ps1'"
-protectedSettings='{"fileUris": "['$fileUris']", "commandToExecute": "'$commandToExecute'" }'
-echo $protectedSettings
-
 az vm extension set -n CustomScriptExtension \
     --publisher Microsoft.Compute --version 1.10 \
-    --vm-name vm-devops-1201 --resource-group rg-secure-appsvc-prod \
-    --protected-settings $protectedSettings \
-    --verbose
-
-az vm extension set -n CustomScriptExtension \ 
-    --publisher Microsoft.Compute --version 1.10 \    
-    --vm-name vm-devops-1201 --resource-group rg-secure-appsvc-prod \
-    --protected-settings '{"fileUris": "['https://raw.githubusercontent.com/Azure/appservice-landing-zone-accelerator/main/scenarios/shared/scripts/win-devops-vm-extensions/post-deployment.ps1']", "commandToExecute": "powershell.exe -ExecutionPolicy Unrestricted -File post-deployment.ps1 -az_cli_commands \"az version\" -install_ssms" }' \
-    --verbose
+	--vm-name vm-lza-app-beta-win-jumpbox \
+	--resource-group rg-spoke-lza-app-beta-dev-francecentral \
+	--settings '{"fileUris": ["https://raw.githubusercontent.com/Azure/appservice-landing-zone-accelerator/feature/external-outputs/scenarios/shared/scripts/win-devops-vm-extensions/post-deployment.ps1"]}' \
+	--protected-settings  '{"commandToExecute": "powershell.exe -ExecutionPolicy Unrestricted -File post-deployment.ps1 -install_cli" }' \
+	--verbose
